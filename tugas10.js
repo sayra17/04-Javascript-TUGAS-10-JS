c30028eceea137eed8c3a55d987dfd227f44da84
sayra17 <agussayra03@gmail.com>
2022-02-03 19:09:16 +0700

    Initial commit

---
+++ tugas10.js
@@ -0,0 +1,11 @@
+
+function pengulangan() {
+    var count = 1;
+    for (var x = 1; x <= 10; x++) {
+        console.log(count);
+        count++;
+        count = count + x;
+    }
+}
+
+pengulangan();
